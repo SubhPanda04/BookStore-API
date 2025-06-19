d7489c5a0214db2468efc80ca4af537e74ccd36b
Subhasis Panda <subhasispanda1256700@gmail.com>
2025-06-19 18:51:45 +0530

    Added env

--- Database/database.js
+++ Database/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectToDB = async()=> {
     try {
-        await mongoose.connect("mongodb+srv://subhasispanda1256700:1234567Aa@cluster0.f6tw1.mongodb.net/");
+        await mongoose.connect(process.env.MONGO_URL);
         console.log("Connected to Database");
     } catch (e) {
         console.log("Connection Failed",e);
